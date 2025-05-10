c04834246f86b9683589799ec8d95b17f9e4227a
Gabriel Santos <gabriel.santos@dev.otimize.me>
2025-05-09 22:55:54 -0300

    chore(deps): add pest and pinia to project, remove phpunit

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createPinia } from 'pinia';
 
 createInertiaApp({
   resolve: (name) =>
@@ -9,6 +10,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(createPinia())
       .mount(el);
   },
 });
